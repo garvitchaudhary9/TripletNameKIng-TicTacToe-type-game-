e1ee74ff00b2efc122225bcdbcf1e4b39b4b7a57
chaudharygarvit9 <chaudharygarvit9@gmail.com>
2020-10-26 15:58:28 +0530

    Initial Commit

---
+++ TripletNameKIng(TicTacToe type game)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TripletNameKIng(TicTacToe type game)
+//
+//  Created by Garvit Chaudhary on 26/10/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
